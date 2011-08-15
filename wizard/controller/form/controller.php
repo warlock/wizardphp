cc9d36ce8e81df45cdd78d007b48106e1de92634
Josep Subils <josep@eternal.cat>
2011-08-15 13:07:30 -0400

    Updated form controller

--- wizard/controller/form/controller.php
+++ wizard/controller/form/controller.php
@@ -1,8 +1,8 @@
 <?
 // WizardPHP : Form Controller
-$goto = $wizard_model[_go_to]; // URL that goes after "submit"
-if(isset($goto)) {
-	?><form method="post" action="<? print $goto; ?>"><?
+
+if(isset($wizard_model[_go_to])) { // URL that goes after "submit"
+	?><form method="post" action="<? print $wizard_model[_go_to]; ?>"><?
 } else {
 	$url = $_SERVER['SERVER_NAME']; 
 	$page = $_SERVER['PHP_SELF']; 
@@ -14,7 +14,7 @@ if ($id_update > 0) {
 	$db = &ADONewConnection($wizard_config['class']);
 	$db->PConnect($wizard_config['host'],$wizard_config['user'],$wizard_config['password'],$wizard_config['database']);
 	$result = $db->Execute("select * from ".$wizard_model_name." where id = ".$id_update);
-	$valor = $result->FetchRow(0);
+	$value = $result->FetchRow(0);
 	$db->Close();
 	?><input type="hidden" name="action" value="update"><?
 	?><input type="hidden" name="id" value="<? print $id_update; ?>"><?
@@ -23,65 +23,64 @@ if ($id_update > 0) {
 }
 ?><input type="hidden" name="form_id" value="<? print $wizard_model_name; ?>"><?
 // Check the model and prints the results.
-	foreach ($wizard_model as $sub => $val) {
-		switch ($sub) {
+	foreach ($wizard_model as $key => $class) {
+		switch ($key) {
 			case "_go_to":
 				break;
 			case "_button":
-					if (is_array($val)) {
-						foreach ($val as $sub2 => $val2) {
-								?><input type="<? print $val2; ?>" value="<? trans($wizard_model_name,$val2); ?>"><? 
+					if (is_array($class)) {
+						foreach ($class as $key2 => $class2) {
+								?><input type="<? print $class2; ?>" value="<? trans($wizard_model_name,$class2); ?>"><? 
 						}
 					} else {
-						?><input type="<? print $val; ?>" value="<? trans($wizard_model_name,$val); ?>"><?
+						?><input type="<? print $class; ?>" value="<? trans($wizard_model_name,$class); ?>"><?
 					}
 				break;
 			case "level":
 				// Under construction. Displays the "level" when a user has permissions.
 				break;
 			default:
-				switch ($val) {
+				switch ($class) {
 					case "string":
-						trans($wizard_model_name,$sub); ?> <input type="text" name="<? print $sub; ?>" value="<? print $valor[$sub]; ?>"><br><?
+						trans($wizard_model_name,$key); ?> <input type="text" name="<? print $key; ?>" value="<? if ($id_update > 0) { print $value[$key]; } ?>"><br><?
 						break;
 					case "password":
-						trans($wizard_model_name,$sub); ?> <input type="password" name="<? print $sub; ?>"><? print $valor[$sub] ?><br><?
+						trans($wizard_model_name,$key); ?> <input type="password" name="<? print $key; ?>"><br><?
 						break;
 					case "checkbox":
-						trans($wizard_model_name,$sub); ?> <input type="checkbox" name="<? print $sub; ?>"><? print $sub; ?><br><?
+						// Under construction
 						break;
 					case "text":
-						trans($wizard_model_name,$sub); ?> <textarea name="<? print $sub; ?>" rows="3" cols="20"><? print $valor[$sub] ?></textarea><br><?
+						trans($wizard_model_name,$key); ?> <textarea name="<? print $key; ?>" rows="3" cols="20"><? if ($id_update > 0) { print $value[$key]; } ?></textarea><br><?
 						break;
 					case "wysiwyg":
-						trans($wizard_model_name,$sub);
+						trans($wizard_model_name,$key);
 						?><br><?
 						$CKEditor = new CKEditor();
 						$CKEditor->basePath = 'wizard/core/tools/ckeditor/';
-						$CKEditor->editor($sub, $valor[$sub]);
+						$CKEditor->editor($key, $value[$key]);
 						break;
 					case "radio":
 						// Under construction
-						trans($wizard_model_name,$sub); ?> <input type="radio" name="formradio" value="<? print $sub; ?>"><? print $sub; ?><br><? 
 						break;
 					case "_captcha":
 						// Under construcction
-						/* trans($wizard_model_name,$sub); ?> <img src="<? print captcha(); ?>" name="<? print $sub; ?>"><br><? */
+						/* trans($wizard_model_name,$key); ?> <img src="<? print captcha(); ?>" name="<? print $key; ?>"><br><? */
 						break;
 					case "date":
-						trans($wizard_model_name,$sub); ?> <select name="day-<? print $sub; ?>">
+						trans($wizard_model_name,$key); ?> <select name="day-<? print $key; ?>">
 						<? for($x=1;$x<=31;$x++) { print "<option>".$x; } ?>
 						</select>
-						<select name="month-<? print $sub; ?>">
+						<select name="month-<? print $key; ?>">
 						<? for($x=1;$x<=12;$x++) { print "<option>".$x; } ?>
 						</select>
-						<input type="text" name="year-<? print $sub; ?>" maxlength="4" size="4">
+						<input type="text" name="year-<? print $key; ?>" maxlength="4" size="4">
 						<br><?
 						break;
 					default:
 						// It ensures that there is some direction after removing the row.
-						if ($sub == "_go_destroy") {
-                        	$_go_destroy = $val;
+						if ($key == "_go_destroy") {
+                        	$_go_destroy = $class;
                             $destroylock = 1;
                         } else {
                         	if ($destroylock == 0) {
@@ -94,5 +93,6 @@ if ($id_update > 0) {
 ?></form><?
 // Button to delete the row when editing.
 if ($id_update>0) {
-	?><form method="post" action="<? print $_go_destroy; ?>"><input type="hidden" name="action" value="destroy"><input type="hidden" name="form_id" value="<? print $wizard_model_name; ?>"><input type="hidden" name="id" value="<?print $valor["id"]; ?>"><input type="submit" value="<? trans($wizard_model_name,"_go_destroy"); ?>"></form></th>
+	?><form method="post" action="<? print $_go_destroy; ?>"><input type="hidden" name="action" value="destroy"><input type="hidden" name="form_id" value="<? print $wizard_model_name; ?>"><input type="hidden" name="id" value="<? if ($id_update > 0) { print $value['id']; } ?>"><input type="submit" value="<? trans($wizard_model_name,"_go_destroy"); ?>"></form></th>
+	ID UPDATE ES MES GRAN QUE O???
 <? } ?>
