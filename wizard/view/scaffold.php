c64440ec721fc220e20c82de5f9b3f034b39e5b2
Josep Subils <ninpo85@gmail.com>
2012-05-30 04:15:52 +0200

    Updated scaffold!

--- wizard/view/scaffold.php
+++ wizard/view/scaffold.php
@@ -48,7 +48,6 @@ if(isset($wzd_segmento[$wzd_seg_act[1]]) and $wzd_segmento[$wzd_seg_act[1]] != "
 			break;
 		case 'search':
 			if(user('level') >= $wizard_model_complete[$wzd_segmento[$wzd_model_seg]]['_show'] or $wizard_config['mode'] == "development") { 
-				print "Te permis";		
 			}
 			break;
 		case 'new':
